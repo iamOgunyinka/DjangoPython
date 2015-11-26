ae1b2f2b41ab3030bcfc93daad74023f5eca35e9
Joshua Ogunyinka <ogunyinkajoshua@yahoo.com>
2015-11-26 12:03:12 +0100

    Second and subsequent changes made

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,20 @@
-from selenium import webdriver
+from selenium import webdriver as wb
+import unittest
 
-browser = webdriver.Chrome('C:\Downloads\chromedriver')
-browser.get( "http://127.0.0.1:8000" )
-assert "Django" in browser.title
+class NewVisitorTest( unittest.TestCase ):
+    def setUp( self ):
+        self.browser = wb.Chrome( 'C:\Downloads\chromedriver' )
+    
+    def tearDown( self ):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later( self ):
+        self.browser.get( 'http://127.0.0.1:8000' )
+        
+        self.assertIn( 'To-Do', self.browser.title )
+        self.fail( 'Finish the test' )
+        
+        
+
+if __name__ == "__main__":
+    unittest.main( warnings = 'ignore' )

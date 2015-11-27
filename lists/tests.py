ebc4da5f3b63bccb9685ea8f1304b8f78975b916
Joshua Ogunyinka <ogunyinkajoshua@yahoo.com>
2015-11-27 13:40:13 +0100

    Generated the homepage from the default templates

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,18 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
+from lists.views import home_page
+from django.http import HttpRequest
 
-class SmokeTest( TestCase ):
-    def test_bad_math( self ):
-        self.assertEqual( 1 + 2 , 4 )
+class HomePageTest( TestCase ):
 
+  def test_root_url_resolves_to_home_page_view( self ):
+    found = resolve( '/' )
+    self.assertEqual( found.func, home_page )
+
+
+  def test_home_page_returns_correct_html( self ):
+    request = HttpRequest()
+    response = home_page( request )
+    expected_html = render_to_string( 'home.html' )
+    self.assertEqual( response.content.decode(), expected_html )
